c2eae238fe8f1ad9567c8b0cbcd437c46efbcadd
greedmonkey <greedmonkey@greedmonkey-Satellite-L745>
2013-10-30 17:02:41 +0700

    Start project

---
+++ src/com/project/alcoholproject/Book.java
@@ -0,0 +1,104 @@
+package com.project.alcoholproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v4.app.NavUtils;
+
+public class Book extends Activity  {
+
+
+	
+	
+	SharedPreferences shared_preferences;
+	SharedPreferences.Editor shared_preferences_editor;
+	String test_string = "";
+	
+	public String myString[] = {"Kreang","Pea","Da"};
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ImageButton yourBitmap = (ImageButton) findViewById(R.id.plus);
+		
+//		TextView test = (TextView) findViewById(R.id.)
+//		Bitmap resized = Bitmap.createScaledBitmap(yourBitmap, newWidth, newHeight, true);
+		ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_list_item, myString);
+		ListView listView = (ListView) findViewById(R.id.testView1);
+		listView.setAdapter(adapter);
+
+//		shared_preferences = getSharedPreferences("shared_preferences_test",MODE_PRIVATE);
+//		test_string = shared_preferences.getString("test_key1", "Default");
+//		Toast.makeText(getApplicationContext(), test_string, Toast.LENGTH_SHORT).show();
+
+		 
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+	
+	
+	
+	public void onPlus(View view){
+		Intent add_book = new Intent(this, AddBook.class);
+		startActivity(add_book);
+	}
+
+	// public class MyPerformanceArrayAdapter extends ArrayAdapter<String>{
+	//
+	// private final Activity context;
+	// private final String[] listFace;
+	//
+	// public MyPerformanceArrayAdapter(Activity context, String[] listFace) {
+	// this.context = context;
+	//
+	// // TODO Auto-generated constructor stub
+	// }
+	//
+	// }
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.book, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
--- src/com/project/alcoholproject/MainActivity.java
+++ src/com/project/alcoholproject/MainActivity.java
@@ -2,21 +2,57 @@ package com.project.alcoholproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	public static final String PREFS_NAME = "MyPrefsFile";
+	public static final String GAME_PREFERENCES = "GamePrefs";
+	SharedPreferences settings; 
+	SharedPreferences.Editor prefEditor;
+	
+
+	SharedPreferences shared_preferences;
+	SharedPreferences.Editor shared_preferences_editor;
+	String test_string = "";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		
+	    
+	    shared_preferences = getSharedPreferences("shared_preferences_test",
+	            MODE_PRIVATE);
+	    test_string = shared_preferences.getString("test_key", "Default");
+
+	    shared_preferences_editor = shared_preferences.edit();
+
+	    shared_preferences_editor.putString("test_key1", "Kreang");
+	    shared_preferences_editor.putString("test_key2", "Pea");
+	    shared_preferences_editor.putString("test_key3", "Da");
+	    shared_preferences_editor.commit();
+		  
+		  setContentView(R.layout.activity_main);
+		
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void bookClick(View view){
+		Intent book = new Intent(this, Book.class);
+		startActivity(book);
 	}
 
 }
