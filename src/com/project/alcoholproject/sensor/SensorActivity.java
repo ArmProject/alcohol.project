5b40d3d8cb5c8a8fc5b622ba09a9a5524afdb5b4
arm <arm@arm>
2013-10-31 17:10:25 +0700

    Finish add sensor

--- src/com/project/alcoholproject/sensor/SensorActivity.java
+++ src/com/project/alcoholproject/sensor/SensorActivity.java
@@ -1,28 +1,55 @@
-package com.project.alcoholproject.line;
+package com.project.alcoholproject.sensor;
 
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.WakeLockOptions;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.opengl.font.FontFactory;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.IGameInterface.OnCreateResourcesCallback;
+import org.andengine.ui.IGameInterface.OnCreateSceneCallback;
+import org.andengine.ui.IGameInterface.OnPopulateSceneCallback;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.call.Callback;
 import org.andengine.util.color.Color;
 
-import android.graphics.Typeface;
-import android.util.Log;
+import com.project.alcoholproject.R;
+import com.project.alcoholproject.line.Algorithm;
+import com.project.alcoholproject.line.PointFactory;
+import com.project.alcoholproject.line.ResourceManager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class LineActivity extends BaseGameActivity {
+public class SensorActivity extends BaseGameActivity {
+
 	private static final int CAMERA_WIDTH = 800;
 	private static final int CAMERA_HEIGHT = 480;
 
 	private Camera camera;
+	float k = 5;
+	TextView txtShow, txtStat;
+
+	// @Override
+	// protected void onCreate(Bundle pSavedInstanceState) {
+	// super.onCreate(pSavedInstanceState);
+	// SensorManager sensorManager = (SensorManager)
+	// getSystemService(Context.SENSOR_SERVICE);
+	// Sensor sensor = sensorManager
+	// .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	// sensorManager.registerListener(, sensor,
+	// SensorManager.SENSOR_DELAY_NORMAL);
+	// }
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -39,12 +66,6 @@ public class LineActivity extends BaseGameActivity {
 	public void onCreateResources(
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws Exception {
-		float size = 40;
-		ResourceManager res = new ResourceManager(
-				this.getVertexBufferObjectManager());
-		res.setFont(FontFactory.createFromAsset(this.getFontManager(),
-				this.getTextureManager(), 256, 256, this.getAssets(),
-				"font/droid.ttf", size, true, android.graphics.Color.WHITE));
 
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
@@ -54,29 +75,23 @@ public class LineActivity extends BaseGameActivity {
 			throws Exception {
 		Scene scene = new Scene();
 		scene.setBackground(new Background(Color.WHITE));
-		
-		int n = 16;
-		int d = 4;
 
-		final Algorithm algo = new Algorithm(d, n);
+		PointManager point = new PointManager(scene,
+				this.getVertexBufferObjectManager());
+		point.createPoint(CAMERA_WIDTH, CAMERA_HEIGHT);
+		point.setOnFinish(new Callback<Object>() {
 
-		final PointFactory factory = new PointFactory(scene);
-
-		factory.random(algo.getData(), d, n, CAMERA_WIDTH, CAMERA_HEIGHT);
-		factory.setOnTop(new Callback<Object>() {
-			public void onCallback(Object obj) {
-				int index = (Integer) obj;
-				// Log.e("aaa", index + "");
-				boolean stat = algo.check(index);
-				// Log.e("bbb", stat + "");
-				if (!stat) {
-					factory.finish();
-				} else {
-					algo.set(index);
-				}
-
-			};
+			@Override
+			public void onCallback(Object pCallbackValue) {
+				Toast.makeText(SensorActivity.this, "You lose!!!",
+						Toast.LENGTH_LONG).show();
+			}
 		});
+		SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		Sensor sensor = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorManager.registerListener(point, sensor,
+				SensorManager.SENSOR_DELAY_UI);
 
 		pOnCreateSceneCallback.onCreateSceneFinished(scene);
 	}
